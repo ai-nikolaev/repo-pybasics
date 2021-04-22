3fce32e4923ecc64eb5c626d3e9b69a0910f1e9f
Aleksandr Nikolaev <ai.nikolaev@mail.ru>
2021-04-23 00:36:22 +0300

    added home work for the first three exercises

--- homework/lesson-2/exercise-1/main.py
+++ homework/lesson-2/exercise-1/main.py
@@ -1,13 +1,33 @@
-elements = [1, 'A', "ABC", 2.0, None]
-print(f'Список: {elements}')
-for e in elements:
-    if type(e) is int:
-        print(f'Тип данных элемента списка "{e}" - "{int}".')
-    elif type(e) is float:
-        print(f'Тип данных элемента списка "{e}" - "{float}".')
-    elif type(e) is str:
-        print(f'Тип данных элемента списка "{e}" - "{str}".')
-    elif type(e) is None:
-        print(f'Тип данных элемента списка "{e}" - "{None}".')
+# создание пустого списка
+my_list = []
+
+# ввод количества элементов списка
+n = int(input("Введите количество элементов списка: "))
+
+# наполнение списка
+for i in range(n):
+    value = input(f'Введите {i+1}-й элемент списка: ')
+    if value == 'None':
+        value = None
+    elif value.isnumeric():
+        value = int(value)
     else:
-        print(f'Тип данных элемента списка "{e}" - "{type(e)}".')
+        try:
+            value = float(value)
+        except ValueError:
+            pass
+    my_list.append(value)
+
+# вывод списка
+print(f'Список элементов: {my_list}')
+
+# разбор типов
+for e in my_list:
+    if isinstance(e, int):
+        print(f'Тип данных элемента "{e}" - "int"')
+    elif isinstance(e, float):
+        print(f'Тип данных элемента "{e}" - "float"')
+    elif isinstance(e, str):
+        print(f'Тип данных элемента "{e}" - "str"')
+    else:
+        print(f'Тип данных элемента "{e}" - "{type(e)}"')

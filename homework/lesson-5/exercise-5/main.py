6ed3c5d5b9dce85c095bdfacb9030298880b3fc9
Aleksandr Nikolaev <ai.nikolaev@mail.ru>
2021-05-01 15:46:06 +0300

    added the exercise-5

---
+++ homework/lesson-5/exercise-5/main.py
@@ -0,0 +1,16 @@
+# 5. Создать (программно) текстовый файл, записать в него программно
+# набор чисел, разделенных пробелами. Программа должна подсчитывать
+# сумму чисел в файле и выводить ее на экран.
+
+with open("text_file.txt", "w") as f_obj1:
+    while True:
+        try:
+            value = int(input("Введите число: "))
+            print(value, end = ' ', file = f_obj1)
+        except ValueError:
+            print('Значение не является числом. Ввод завершается.')
+            break
+        
+with open("text_file.txt") as f_obj2:
+    for line in f_obj2:
+        print(f'Сумма чисел в файле: {sum([int(a) for a in line.split()])}')

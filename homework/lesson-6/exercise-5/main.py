b7173b61ff5404d5da4ebf3a51998bec4d243a3e
Aleksandr Nikolaev <ai.nikolaev@mail.ru>
2021-05-11 22:18:28 +0300

    added the exercise-5

---
+++ homework/lesson-6/exercise-5/main.py
@@ -0,0 +1,47 @@
+# 5. Реализовать класс Stationery (канцелярская принадлежность).
+# Определить в нем атрибут title (название) и метод draw (отрисовка).
+# Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних
+# класса Pen (ручка), Pencil (карандаш), Handle (маркер).
+# В каждом из классов реализовать переопределение метода draw.
+# Для каждого из классов методы должен выводить уникальное сообщение.
+# Создать экземпляры классов и проверить, что выведет описанный
+# метод для каждого экземпляра.
+
+class Stationery:
+    title = ''
+    
+    def draw(self):
+        print('Запуск отрисовки.')
+        
+class Pen(Stationery):
+    
+    def __init__(self, title):
+        self.title = title
+    
+    def draw(self):
+        print(f'Пишу текст с помощью ручки "{self.title}".')
+
+class Pencil(Stationery):
+    
+    def __init__(self, title):
+        self.title = title
+    
+    def draw(self):
+        print(f'Рисую чертеж с помощью карандаша "{self.title}".')
+
+class Handle(Stationery):
+    
+    def __init__(self, title):
+        self.title = title
+    
+    def draw(self):
+        print(f'Делаю заметки с помощью маркера "{self.title}".')
+
+my_item1 = Pen('Лучшая ручка')
+my_item2 = Pencil('Великолепный карандаш')
+my_item3 = Handle('Самый лучший маркер')
+
+my_item1.draw()
+my_item2.draw()
+my_item3.draw()
+
